18c4171bcd97735d3851efb5af1f455bf62499d6
Mariano de Achaval <mariano.achaval@mulesoft.com>
2013-11-05 15:09:45 -0300

    Shade MVEL

---
+++ src/test/java/org/mule/mvel2/tests/core/ProjectionsTests.java
@@ -0,0 +1,51 @@
+package org.mule.mvel2.tests.core;
+
+import org.mule.mvel2.MVEL;
+import org.mule.mvel2.tests.core.res.Base;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+
+public class ProjectionsTests extends AbstractTest {
+  public void testProjectionSupport() {
+    assertEquals(true, test("(name in things)contains'Bob'"));
+  }
+
+  public void testProjectionSupport1() {
+    assertEquals(true, test("(name in things) contains 'Bob'"));
+  }
+
+  public void testProjectionSupport2() {
+    String ex = "(name in things).size()";
+    Map vars = createTestMap();
+
+    assertEquals(3, MVEL.eval(ex, new Base(), vars));
+
+    assertEquals(3, test("(name in things).size()"));
+  }
+
+  public void testProjectionSupport3() {
+    String ex = "(toUpperCase() in ['bar', 'foo'])[1]";
+    Map vars = createTestMap();
+
+    assertEquals("FOO", MVEL.eval(ex, new Base(), vars));
+
+    assertEquals("FOO", test("(toUpperCase() in ['bar', 'foo'])[1]"));
+  }
+
+  public void testProjectionSupport4() {
+    Collection col = (Collection) test("(toUpperCase() in ['zero', 'zen', 'bar', 'foo'] if ($ == 'bar'))");
+    assertEquals(1, col.size());
+    assertEquals("BAR", col.iterator().next());
+  }
+
+  public void testProjectionSupport5() {
+    Collection col = (Collection) test("(toUpperCase() in ['zero', 'zen', 'bar', 'foo'] if ($.startsWith('z')))");
+    assertEquals(2, col.size());
+    Iterator iter = col.iterator();
+    assertEquals("ZERO", iter.next());
+    assertEquals("ZEN", iter.next());
+  }
+
+}
