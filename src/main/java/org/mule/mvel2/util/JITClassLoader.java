abd54fb52a5bfd6c954c48ad410c38ce26656807
jcgrethe <jgrethe@itba.edu.ar>
2021-05-31 16:11:53 -0300

    tes

--- src/main/java/org/mule/mvel2/util/JITClassLoader.java
+++ src/main/java/org/mule/mvel2/util/JITClassLoader.java
@@ -44,6 +44,11 @@ public class JITClassLoader extends ClassLoader implements MVELClassLoader {
   }
 
   public Class<?> defineClassX(String className, byte[] b, int off, int len) {
-    return super.defineClass(className, b, off, len);
+    if (sunJVM) {
+      return ((Unsafe) sunUnsafe).defineClass(className, b, off, len);
+    }
+    else {
+      return super.defineClass(className, b, off, len);
+    }
   }
 }
