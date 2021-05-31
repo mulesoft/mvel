3c890cfcb2215861a2be701ef412db4d1852b572
jcgrethe <jgrethe@itba.edu.ar>
2021-05-31 16:15:12 -0300

    remove the method again

--- src/main/java/org/mule/mvel2/util/JITClassLoader.java
+++ src/main/java/org/mule/mvel2/util/JITClassLoader.java
@@ -44,11 +44,6 @@ public class JITClassLoader extends ClassLoader implements MVELClassLoader {
   }
 
   public Class<?> defineClassX(String className, byte[] b, int off, int len) {
-    if (sunJVM) {
-      return ((Unsafe) sunUnsafe).defineClass(className, b, off, len);
-    }
-    else {
-      return super.defineClass(className, b, off, len);
-    }
+    return super.defineClass(className, b, off, len);
   }
 }
